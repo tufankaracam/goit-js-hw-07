99a03e6fe4f69c961de54560006f3fb8e3a97685
tufankaracam <tufankaracam@hotmail.com.tr>
2024-05-12 16:30:35 +0200

    with css

--- js/task-6.js
+++ js/task-6.js
@@ -16,6 +16,7 @@ const createBoxes = (amount) => {
   const boxes = [];
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
+    box.classList.add('box');
     box.style.width = `${30 + i * 10}px`;
     box.style.height = `${30 + i * 10}px`;
     box.style.backgroundColor = getRandomHexColor();
